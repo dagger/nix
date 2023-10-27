284deb135b4c08bcc4e70e935c91ab1619826092
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-10-27 20:18:54 +0200

    clear repository from template contents
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- default.nix
+++ default.nix
@@ -13,8 +13,4 @@
   lib = import ./lib { inherit pkgs; }; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
-
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
 }
